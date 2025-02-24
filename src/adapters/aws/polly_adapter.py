8a32c93fbaa6e63b9acd594fb21b3d2433419aa5
OmarKhaled0K <omarkhaledcvexpert@gmail.com>
2025-02-24 16:56:15 +0200

    Use Arabic gulf in polly

--- src/adapters/aws/polly_adapter.py
+++ src/adapters/aws/polly_adapter.py
@@ -1,6 +1,7 @@
 import boto3
 from src.core.config import settings
 import time
+
 class PollyAdapter:
     def __init__(self):
         self.client = boto3.client(
@@ -13,15 +14,23 @@ class PollyAdapter:
     def synthesize_speech(self, text: str) -> str:
         is_arabic = any('\u0600' <= char <= '\u06FF' for char in text)
         
-        voice_id = settings.AWS_POLLY_VOICE_AR if is_arabic else settings.AWS_POLLY_VOICE_EN
-        engine = 'neural' if voice_id == 'Joanna' else 'standard'
-        
+        # Configure for Arabic Gulf (Neural) with Zayd
+        if is_arabic:
+            voice_id = 'Zayd'  # Arabic (Gulf) male neural voice
+            language_code = 'ar-AE'  # Gulf Arabic
+            engine = 'neural'
+        else:
+            # Default to English neural voice (e.g., Joanna)
+            voice_id = settings.AWS_POLLY_VOICE_EN
+            language_code = 'en-US'
+            engine = 'neural'
+
         response = self.client.synthesize_speech(
             Text=text,
             OutputFormat='mp3',
             VoiceId=voice_id,
             Engine=engine,
-            LanguageCode='ar-SA' if is_arabic else 'en-US'
+            LanguageCode=language_code
         )
         
         s3 = boto3.client('s3')
@@ -29,7 +38,9 @@ class PollyAdapter:
         s3.put_object(
             Bucket=settings.AWS_S3_BUCKET_NAME,
             Key=file_name,
-            Body=response['AudioStream'].read()
+            Body=response['AudioStream'].read(),
+            ACL='public-read',
+            ContentType='audio/mpeg'
         )
         
         return f"https://{settings.AWS_S3_BUCKET_NAME}.s3.{settings.AWS_REGION}.amazonaws.com/{file_name}"
